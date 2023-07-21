237edb1bd58a2072dbeabdf7ad458a2dfa51b735
SAKSHAM <61020145+sakshamsaxena06984@users.noreply.github.com>
2023-07-21 14:26:39 +0530

    added spark-sql

--- pair-rdd/src/main/java/org/example/BoringWordCount.java
+++ pair-rdd/src/main/java/org/example/BoringWordCount.java
@@ -4,12 +4,14 @@ import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
 import org.apache.spark.SparkContext;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class BoringWordCount{
     @SuppressWarnings("resource")
@@ -26,14 +28,36 @@ public class BoringWordCount{
         JavaRDD<String> filter = map.filter(sentance -> sentance.trim().length() > 0);
         JavaRDD<String> stringJavaRDD = filter.flatMap(s -> Arrays.asList(s.split(" ")).iterator());
 //        JavaRDD<String> filter1 = stringJavaRDD.filter(w -> Utils.isNotBoring(w)); // will check , why Utils class is not working
-        stringJavaRDD.mapToPair(e-> new Tuple2<String,Long>(e,1L))
-                      .reduceByKey((a,b)->a+b)
-                      .mapToPair(t-> new Tuple2<>(t._2(),t._1()))
-                      .sortByKey(false)
-                      .coalesce(1)
-                      .foreach(e-> System.out.println(e));
+
+        //in below section code, we sorted the rdd records in descending order
+//        stringJavaRDD.mapToPair(e-> new Tuple2<String,Long>(e,1L))
+//                      .reduceByKey((a,b)->a+b)
+//                      .mapToPair(t-> new Tuple2<>(t._2(),t._1()))
+//                      .sortByKey(false)
+//                      .coalesce(1);
+
+        JavaPairRDD<Long, String> longStringJavaPairRDD = stringJavaRDD.mapToPair(e -> new Tuple2<String, Long>(e, 1L))
+                .reduceByKey((a, b) -> a + b)
+                .mapToPair(t -> new Tuple2<>(t._2(), t._1()))
+                .sortByKey(false);
+
+        // way of getting number of partition in rdd
+        System.out.println("Number of partitions in RDD "+longStringJavaPairRDD.getNumPartitions());
+        longStringJavaPairRDD.coalesce(longStringJavaPairRDD.getNumPartitions())
+                        .foreach(e-> System.out.println(e));
+        // printing with sout
 
         System.out.println("---------after replace unnecessary alphabet with space----------");
+        System.out.println(longStringJavaPairRDD.take(100));
+
+
+        Scanner s=new Scanner(System.in);
+        s.nextLine();
+
+
+
+
+
 //        stringJavaRDD.foreach(e-> System.out.println(e));
 
 
